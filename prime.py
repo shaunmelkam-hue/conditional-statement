597a7efd6b9fbdf70e13cf384e1b32d0a6ae5b44
shaunmelkam-hue <shaunmelkam@gmail.com>
2026-07-18 17:57:15 -0500

    Add files via upload

---
+++ prime.py
@@ -0,0 +1,8 @@
+number = int(input("Enter number to check: "))
+print("number to be checked: ", number)
+
+if number%2 == 0:
+    print("The number is even")
+
+else:
+    print("The number is odd")
