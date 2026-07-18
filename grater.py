5fba93fe4ad2ee57ed15f5ec531be22aa4555426
shaunmelkam-hue <shaunmelkam@gmail.com>
2026-07-18 17:52:30 -0500

    Add files via upload

---
+++ grater.py
@@ -0,0 +1,8 @@
+i = int(input("Enter a number: "))
+if (i < 15):
+    print("i is smaller than 15")
+    print("i'm in the if block")
+else:
+    print("i is greater than or equal to 15")
+    print("i'm in the else block")
+print("i'm not in the if or else block")
