719e31c2424bf29f58bdd6bc8956b17fdf93b6a3
shaunmelkam-hue <shaunmelkam@gmail.com>
2026-08-02 17:43:15 -0500

    Add files via upload

---
+++ dimonepat.py
@@ -0,0 +1,10 @@
+n= 5
+
+for i in range(n):
+    print(" " * (n - i - 1), end="")
+    print("*" * (2 * i + 1))
+
+for i in range(n-2, -1, -1):
+    print(" " * (n - i - 1), end="")
+
+    print("*" * (2 * i + 1))
